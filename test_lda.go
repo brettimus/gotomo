c2929827fc3d1abf379f20ed1093f800526bd968
Brett <brbeut@gmail.com>
2014-08-02 22:06:31 -0700

    uncommented useless test function

--- test_lda.go
+++ test_lda.go
@@ -1,33 +1,29 @@
 package gotomo
 
-// import "fmt"
-// import "os"
+import "fmt"
+import "os"
 import "testing"
 
 // func TestNewLdaModel() {
 
 // }
 
-// func TestStuff(t *testing.T) {
-// 	var ds1 = DocSet{Path:"test/", GlobalWordMap: make(map[string]int)}
-// 	var ds2 = DocSet{Path:"test/", GlobalWordMap: make(map[string]int)}
-// 	var wd, _ = os.Getwd()
-//   	var stopWords = Document{File: wd + "/engStopWords.txt", WordMap: make(map[string]int)}
-// 	stopWords.ReadFile()
-// 	// Pass GetFiles() a Nil Document to remove no words. 
-// 	ds1.GetFiles(NewDocument())
-// 	// Pass a Document containing stop words to remove them from docs. 
-// 	ds2.GetFiles(&stopWords)
-// 	fmt.Println(ds1)
-// 	fmt.Println(ds2)
-// 	ds2.Update(&ds1)
-// 	fmt.Println(ds2)
-// 	lda1 := NewLdaModel(ds2, 10, 0.1, 0.1)
-// 	fmt.Println(lda1)
-// 	lda := *lda1
-// 	fmt.Println(lda.Lambda)
-// }
-
-func TestTest(t *testing.T) {
-	Test()
+func TestStuff(t *testing.T) {
+	var ds1 = DocSet{Path:"test/", GlobalWordMap: make(map[string]int)}
+	var ds2 = DocSet{Path:"test/", GlobalWordMap: make(map[string]int)}
+	var wd, _ = os.Getwd()
+  	var stopWords = Document{File: wd + "/engStopWords.txt", WordMap: make(map[string]int)}
+	stopWords.ReadFile()
+	// Pass GetFiles() a Nil Document to remove no words. 
+	ds1.GetFiles(NewDocument())
+	// Pass a Document containing stop words to remove them from docs. 
+	ds2.GetFiles(&stopWords)
+	fmt.Println(ds1)
+	fmt.Println(ds2)
+	ds2.Update(&ds1)
+	fmt.Println(ds2)
+	lda1 := NewLdaModel(ds2, 10, 0.1, 0.1)
+	fmt.Println(lda1)
+	lda := *lda1
+	fmt.Println(lda.Lambda)
 }
