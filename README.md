d7cd30cc683d69f27912d15a7b9187de7669490e
Jacob Menick <jmenick@reed.edu>
2014-08-02 19:33:10 -0700

    update README and add some funcs to lda.go

--- README.md
+++ README.md
@@ -3,21 +3,12 @@ gotomo
 lda in golang
 
 ## TODO
-1. Implement DiGamma Function.
-2. Implement Batch and Online Variational Inference
-3. (lower priority) Have parse function remove punctuation. 
+1. Implement Batch Variational Bayes
+2. Refactor Inference helper funcs. 
+3. Test on a corpus (like patents?)
+4. Implement Online Variational Bayes
 
-3. Across the program, draw distinction between 'batch' and 'update' routines.
-  * For instance, an 'update' method would add documents to a DocSet and increment the appropriate counts.
-	* Similarly, for the eventual inference package, we will have functionality for 'batch' and 'online' inference.
-  * Presumably, the 'online' or 'update' routine will take as input a pointer to a set of update documents.
-  * It will then update the model parameters without iterating through the entire corpus to date. 
 
-4. Should DocSet.Docs be a []Document or a []*Document?
-5. Global Word Count (GWC)
-  * The way JM implemented it, the GWC map is populated when files are read in.
-	* It is updated by merging with another docSet. 
-	* There maybe is a better way to do this... 
-
-### LITTLE THING(S) (-BB)
-1. math pkg: check Log1P vs. Log
+### LITTLE THING(S)
+1. math pkg: check Log1P vs. Log (-BB)
+2. have parse function remove punctuation (-JM)
--- lda.go
+++ lda.go
@@ -37,13 +37,12 @@ func allOnesSlice(size int) []float64 {
 	return out
 }
 
-// batch inference takes an initial DocSet and parameters, returns ptr to LdaModel
-func NewLdaModel(ds DocSet, k int, alpha, beta float64) *LdaModel {
+func NewLdaModel(ds DocSet, K int, alpha, beta float64) *LdaModel {
 	m := len(ds.Docs)
 	v := len(ds.GlobalWordMap)
 	
 	// initialize Lambda Randomly
-	initLambda := make([][]float64, k)
+	initLambda := make([][]float64, K)
 	for i, _ := range initLambda {
 		initLambda[i] = unifRandomSlice(v)
 	}
@@ -51,7 +50,7 @@ func NewLdaModel(ds DocSet, k int, alpha, beta float64) *LdaModel {
 	// initialize gamma with all ones. 
 	initGamma := make([][]float64, m)
 	for i, _ := range initGamma {
-		initGamma[i] = allOnesSlice(k)
+		initGamma[i] = allOnesSlice(K)
 	}
 
 	// initialize VarPhi with all zeroes. 
@@ -59,47 +58,71 @@ func NewLdaModel(ds DocSet, k int, alpha, beta float64) *LdaModel {
 	for i, _ := range initVarPhi {
 		initVarPhi[i] = make([][]float64, v)
 		for j, _ := range initVarPhi[i] {
-			initVarPhi[i][j] = make([]float64, k)
+			initVarPhi[i][j] = make([]float64, K)
 		}
 	}
 
-	ldam :=  LdaModel{Dset: ds, Alpha: alpha, Beta: beta, K: k, Lambda: initLambda, Gamma: initGamma, VarPhi: initVarPhi}
+	ldam :=  LdaModel{Dset: ds, Alpha: alpha, Beta: beta, K: K, Lambda: initLambda, Gamma: initGamma, VarPhi: initVarPhi}
 	return &ldam
 }
 
 // To be called on initialization.
 func (ldam *LdaModel) BatchInfer() {
+	// While not converged, 
+	//   E-Step then M-Step
 }
 
 // online inference is a method on an Lda Model for updating. 
 func (ldam *LdaModel) OnlineInfer(ds *DocSet, kappa, tau float64, batchSize int) {}
 
-func (ldam *LdaModel) thetaExpectation(d, k int) float64{
+func (ldam *LdaModel) ThetaExpectation(d, k int) float64{
 	return digamma(ldam.Gamma[d][k]) - digamma(sumSlice(ldam.Gamma[d]))
 }
 
-func (ldam *LdaModel) phiExpectation(k, t int) float64{
+func (ldam *LdaModel) PhiExpectation(k, t int) float64{
 	return digamma(ldam.Lambda[k][t]) - digamma(sumSlice(ldam.Lambda[k]))
 }
 
-func (ldam *LdaModel) varPhiUpdate(d, t, k int) {}
+// E-Step helpers
+func (ldam *LdaModel) VarPhiUpdateBatch(d, t, k int) {}
+func (ldam *LdaModel) GammaUpdateBatch(d, k int) {}
+
+// M-Step helper
+func (ldam *LdaModel) LambdaUpdateBatch(k, t int) {}
+
+func (ldam *LdaModel) EStepBatch() {}
+func (ldam *LdaModel) MStepBatch() {}
 
 func (ldam *LdaModel) EstParams() ([][]float64, [][]float64) {
 	// returns Topic-Term Probabilities (Phi) and Doc-Topic Mixture Proportions (Theta)
-	m, v := len(ldam.Dset.Docs), len(ldam.Dset.GlobalWordMap)
-	k := ldam.K
-	Phi, Theta := make([][]float64, k), make([][]float64, m) // kept vars here for clarity's sake, but it'd be more efficient just to return without declaring (BB)
+	M, V := len(ldam.Dset.Docs), len(ldam.Dset.GlobalWordMap)
+	numTopics := ldam.K
+	Phi, Theta := make([][]float64, numTopics), make([][]float64, M) // kept vars here for clarity's sake, but it'd be more efficient just to return without declaring (BB)
 
 	// Phi contains slices of length v
 	for i, _ := range Phi {
-		Phi[i] = make([]float64, v)
+		Phi[i] = make([]float64, V)
 	}
 
-	// Theta contains slices of length k
+	// Theta contains slices of length 'numTopics'
 	for i, _ := range Theta {
-		Theta[i] = make([]float64, k)
+		Theta[i] = make([]float64, numTopics)
 	}
 
+	// Assign each entry of Phi to its expecation.
+	for k :=0; k < numTopics; k++ {
+		for t := 0; t < V; t++ {
+			Phi[k][t] = ldam.PhiExpectation(k, t)
+		}
+	}
+
+	// Assign each entry of Theta to its expectation.
+	for d:=0; d < M; d++ {
+		for k:=0; k < numTopics; k++ {
+			Theta[d][k] = ldam.ThetaExpectation(d, k)
+		}
+	}
+	
 	return Phi, Theta
 }
 
