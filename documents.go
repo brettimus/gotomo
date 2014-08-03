9cafc6b00b4f401f2146810ef8786148327c7067
Brett <brbeut@gmail.com>
2014-08-03 00:58:14 -0700

    commiting to things while drunk.

--- documents.go
+++ documents.go
@@ -10,38 +10,70 @@ import(
 )
 
 type DocSet struct {
-	Path           string // path to directory containing the 'Docs'. 
+	Path           string // path to directory containing the 'Docs'. // needs trailing slash
 	Docs           []Document
 	GlobalWordMap  map[string]int
+	StopWordMap    map[string]bool
+	DocsCount      int    // this could be useful for allocating size of Docs + eventual channel (BB)
 }
 
-func (ds DocSet) String() string {
-	const str = "< DocSet: %d Documents in %s, Vocab has %d unique words. >"
-	return fmt.Sprintf(str, len(ds.Docs), ds.Path, len(ds.GlobalWordMap))
+// path requires trailing slash-- parse it?
+func NewDocSet(path string) *DocSet {
+	sw := NewStopWordMap()
+	ds := DocSet{Path: path, StopWordMap: sw}
+	if err := ds.setDocsCount(); err != nil {
+		panic(err)
+	}
+	// pass a channel to GetFiles if we want to read them concurrently (BB)
+	if err := ds.GetFiles(); err != nil {
+		panic(err) // this is panic-worthy, right? (BB)
+	}
+	// TODO merge all of the documents' word maps into the Global wordmap one (BB)
+	return &ds
 }
 
-// This is the 'batch' method to populate a DocSet
-func (ds *DocSet) GetFiles(sw *Document) {
-	dir := ds.Path
-	files, err := ioutil.ReadDir(dir)
-	if err != nil {
-		return
+// I don't like the redundancy between the next two functions.
+// should use closures. halp!! (BB)
+// *** SERIOUSLY USE SOME CLOUSRE *** //
+func (ds *DocSet) setDocsCount() (err error) {
+	var files []os.FileInfo 
+	if files, err = ioutil.ReadDir(ds.Path); err != nil {
+		return err
 	}
 	for _, file := range files {
 		if !file.IsDir() {
-			d := NewDocument()
-			d.File = dir + file.Name()
-			d.ReadFile()
-			d.rmStopWords(sw)
-			for word, count := range d.WordMap { // update global counts. 
-				ds.GlobalWordMap[word] += count
-			}
-			ds.Docs = append(ds.Docs, *d)
+			ds.DocsCount++
 		}
 	}
+	return err
+}
+
+// This is the 'batch' method to populate a DocSet
+//
+func (ds *DocSet) GetFiles() (err error) {
+	files, err := ioutil.ReadDir(ds.Path)
+	if err != nil {
+		return err
+	}
+	for _, file := range files {
+		if !file.IsDir() { // is this necessary? 
+			d := NewDocument(ds)
+			d.File = ds.Path + file.Name()
+			// TODO pass this to go routine as anon func (BB)
+			d.ReadFile()
+
+			// for word, count := range d.WordMap { // update global counts. 
+			// 	ds.GlobalWordMap[word] += count
+			// }
+			ds.Docs = append(ds.Docs, *d)
+		} // handle else of having folders of files?
+	}
+	return err
 }
 
 // Update method for populating a DocSet
+//
+// Consider making this a function that just merges two maps? (BB)
 func (ds *DocSet) Update(other *DocSet) {
 	for _, oDoc := range other.Docs {
 		ds.Docs = append(ds.Docs, oDoc)
@@ -55,27 +87,14 @@ type Document struct {
 	File      string // tag this as: "full path"
 	WordCount int
 	WordMap   map[string]int
+	Parent    *DocSet
 }
 
 // loop through folders and create new documents
-func NewDocument() *Document {
-	var d Document
+func NewDocument(ds *DocSet) *Document {
+	d := Document{Parent: ds}
 	d.WordMap = make(map[string]int)
 	return &d
-}
-
-func (d Document) String() string {
-	const str = "<< Document: \"%s\" [%d Words, %d Unique] >>"
-	return fmt.Sprintf(str,d.File, d.WordCount,len(d.WordMap))
-}
-
-// does it need an error?
-// it's reading '\n' as a word
-func (d *Document) ReadLine(s string) {
-	for _, word := range strings.Split(s, " ") {
-		d.WordMap[parse(word)]++
-		d.WordCount++
-	}
 }
 
 func (d *Document) ReadFile() (err error) {
@@ -99,11 +118,27 @@ func (d *Document) ReadFile() (err error) {
 
 }
 
+// DO we need error handling here? (BB)
+// it's reading '\n' as a word
+func (d *Document) ReadLine(s string) {
+	for _, word := range strings.Split(s, " ") {
+		d.Parse(word)
+	}
+}
+
+func (d *Document) Parse(s string) {
+	word := strings.ToLower(strings.Trim(s,"\n"))
+	if !d.Parent.StopWordMap[word] {
+		d.WordMap[word]++
+		d.WordCount++
+	}
+}
+
+
+
 /*** Parsing Helpers ***/
 
-func parse(s string) string {
-	return strings.ToLower(strings.Trim(s,"\n"))
-}
+
 
 // Remove words that appear in a stopwords doc from d.
 //  
@@ -112,4 +147,17 @@ func (d *Document) rmStopWords(sw *Document) {
 	for key, _ := range sw.WordMap {
 		delete(d.WordMap, key)
 	}
+}
+
+
+/*** String() methods ***/
+
+func (ds DocSet) String() string {
+	const str = "< DocSet: %d Documents in %s, Vocab has %d unique words. >"
+	return fmt.Sprintf(str, len(ds.Docs), ds.Path, len(ds.GlobalWordMap))
+}
+
+func (d Document) String() string {
+	const str = "<< Document: \"%s\" [%d Words, %d Unique] >>"
+	return fmt.Sprintf(str,d.File, d.WordCount,len(d.WordMap))
 }
--- gotomo.go
+++ gotomo.go
@@ -2,24 +2,13 @@ package gotomo
 
 import(
 		"fmt"
-		"os"
 )
 
 func Test() {
-	var ds1 = DocSet{Path:"test/", GlobalWordMap: make(map[string]int)}
-	var ds2 = DocSet{Path:"test/", GlobalWordMap: make(map[string]int)}
-	var wd, _ = os.Getwd()
-  	var stopWords = Document{File: wd + "/engStopWords.txt", WordMap: make(map[string]int)}
-	stopWords.ReadFile()
-	// Pass GetFiles() a Nil Document to remove no words. 
-	ds1.GetFiles(NewDocument())
-	// Pass a Document containing stop words to remove them from docs. 
-	ds2.GetFiles(&stopWords)
+	ds1 := NewDocSet("test/")
+	ds1.GetFiles()
 	fmt.Println(ds1)
-	fmt.Println(ds2)
-	ds2.Update(&ds1)
-	fmt.Println(ds2)
-	lda1 := NewLdaModel(ds2, 10, 0.1, 0.1)
+	lda1 := NewLdaModel(*ds1, 10, 0.1, 0.1)
 	fmt.Println(lda1)
 	lda := *lda1
 	fmt.Println(lda.Lambda)
